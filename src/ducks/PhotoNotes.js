28d51214daa79da90737e812fedb349e8d8e8fc3
sleekvick <nvonweb@outlook.com>
2018-03-20 20:25:23 +0100

    Cache photoNotes

--- src/ducks/PhotoNotes.js
+++ src/ducks/PhotoNotes.js
@@ -3,9 +3,17 @@ import app, { toArray } from 'shared/firebase';
 import { RNS3 } from 'react-native-aws3';
 import { StartRequest, FinishRequest } from 'ducks/request';
 
+const SET_UPLOADED_PHOTOS = 'SET_UPLOADED_PHOTOS';
+
+const initialState = {
+  uploads: [],
+};
+
 const genRandom = () => `${Math.floor(Math.random() * 6)}${Math.floor(Math.random() * 6)}${Math.floor(Math.random() * 6)}${Math.floor(Math.random() * 6)}${Math.floor(Math.random() * 6)}${Math.floor(Math.random() * 6)}`;
 
-export const AddPhotoNote = photoNote => dispatch => new Promise(async (resolve, reject) => {
+export const AddPhotoNote = photoNote => (dispatch, getState) => new Promise(async (resolve, reject) => {
+  const { uploadPhotosState: { uploads } } = getState();
+
   const photosRef = app.database().ref('/photos');
 
   const options = {
@@ -30,6 +38,10 @@ export const AddPhotoNote = photoNote => dispatch => new Promise(async (resolve,
   try {
     photoNote.images = response;
     await photosRef.push(photoNote);
+    dispatch({
+      type: SET_UPLOADED_PHOTOS,
+      uploads: uploads.concat(photoNote)
+    });
     resolve(photoNote);
     dispatch(FinishRequest());
   } catch (err) {
@@ -39,16 +51,40 @@ export const AddPhotoNote = photoNote => dispatch => new Promise(async (resolve,
 });
 
 export const GetPhotoNotes = userId => (dispatch, getState) => new Promise((resolve, reject) => {
+  const { uploadPhotosState: { uploads } } = getState();
+  dispatch(StartRequest());
+
+  if (uploads.length > 1) {
+    resolve(uploads);
+    dispatch(FinishRequest());
+    return;
+  }
+
   const photoRef = app
     .database()
     .ref('/photos')
     .equalTo(userId)
     .orderByChild('userId');
 
-  dispatch(StartRequest());
   photoRef.once('value', snapshot => {
     const photos = toArray(snapshot.val()) || [];
+    dispatch({
+      type: SET_UPLOADED_PHOTOS,
+      uploads: photos
+    });
     resolve(photos);
     dispatch(FinishRequest());
   });
 });
+
+
+export const UploadPhotosReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_UPLOADED_PHOTOS:
+      return { uploads: action.uploads };
+    default:
+      break;
+  }
+
+  return state;
+};
